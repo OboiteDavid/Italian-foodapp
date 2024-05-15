521e2abd2eaf8684b5bd9f40ad94b2e3a4db91d5
OboiteDavid <168584335+OboiteDavid@users.noreply.github.com>
2024-05-15 12:01:10 +0100

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,36 @@
+function openMenu() {
+    document.querySelector('.menu').style.left = '0';
+    document.querySelector('.overlay').style.width = '100%';
+    document.querySelector('.overlay').style.display = 'block';
+    document.querySelector('.menu-icon').style.display = 'none';
+  }
+  
+  function closeMenu() {
+    document.querySelector('.menu').style.left = '-250px';
+    document.querySelector('.overlay').style.width = '0';
+    document.querySelector('.overlay').style.display = 'none';
+    document.querySelector('.menu-icon').style.display = 'block';
+  }
+//  for scroll view fixed
+window.addEventListener('scroll', function() {
+  var menuHeader = document.getElementById('menuHeader');
+  // Corrected the method name from 'getElementByClassName' to 'getElementsByClassName'
+  var menuLink =  document.getElementsByClassName('menu-link-scroll');
+  var scrollHeight = window.scrollY;
+  
+  // Change position of menu header when scroll height is 45 or greater
+  if (scrollHeight >= 45) {
+    menuHeader.classList.add('navbar-dark'); 
+    menuLink.classList.add('menu-link-dark'); 
+  } else {
+    // Removed 'add' and replaced it with 'remove' for class removal
+    menuLink.classList.add('menu-link-white'); 
+  }
+  
+});
+
+
+
+
+
+
